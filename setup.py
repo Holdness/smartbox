41336adb3393ea32c0ac916e4f37921b8aa5f50e
Holdness <135753823+Holdness@users.noreply.github.com>
2024-10-24 19:07:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='smartbox',
-    version="99",
+    version="3.0",
     author="Graham Bennett",
     author_email="graham@grahambennett.org",
     description="Python API to control heating 'smart boxes'",
